d77030308b12f0cd02783cdf64133b7261b494fc
Jamal Deen <mutalajamaldeengbambegu2020@gmail.com>
2023-12-04 10:47:10 +0000

    Add files via upload

---
+++ day_1helloworld.py
@@ -0,0 +1,54 @@
+#level 1
+
+# Question 1 : check the python version
+
+import sys
+print(sys.version)
+
+# Question two performing operation on 3 and 4
+
+print(4 + 3)             # addition(+)
+print(4- 3)             # subtraction(-)
+print(4 * 3)             # multiplication(*)
+print(4 % 3)             # modulus(%)
+print(4 / 3)             # division(/)
+print(4 ** 3)            # exponential(**)
+print(4 // 3)            # Floor division operator(//)
+
+#Question 2
+
+print("my name is MUTALA GBAMBEGU JAMAL DEEN") #Your name
+print("my family name is GBAMBEGU") #Your family name
+print("my country name is GHANA") #Your country
+print("I am enjoying 30 days of python challenge")
+
+#Question 3 checking data type
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Your name'))
+print(type('Your family name'))
+print(type('Your country'))
+
+#level 2 should be checked on helloworld.py file inside day_1 folder
+
+#Exercise: Level 3
+
+#Question 1 
+matric_no = '08148481300'
+guardians =['Mr M','mr M']
+name = 'mohammed mutala'
+jamb_score ={'english':77, 'maths':53}
+state_of_origin = 'ghanas'
+print('My Name is', name, 'i have two guardians namely', guardians, 'I am from', state_of_origin, 'i scored', jamb_score,'in jamb' 
+'And this is my matric number', matric_no)
+
+#Question 2 finding ecludian distance 
+
+import math
+v=(2, 3) 
+h= (10, 8)
+print('the ecludian distance is', math.dist(v,h))
